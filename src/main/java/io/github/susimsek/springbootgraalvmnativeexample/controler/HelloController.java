7976f6a1b896723d137973ed7c55387a95df8f29
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-01-15 22:33:13 +0300

    init commit

--- src/main/java/io/github/susimsek/springbootgraalvmnativeexample/controler/HelloController.java
+++ src/main/java/io/github/susimsek/springbootgraalvmnativeexample/controler/HelloController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1")
-@Tag(name = "hello", description = "Hello API")
+@Tag(name = "hello", description = "Endpoints for Hello World operations")
 public class HelloController {
 
     /**
