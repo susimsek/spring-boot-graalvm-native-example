a6fef68ac0afb7d6f1ee23347c1df6dac8e7430a
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-01-20 14:46:28 +0300

    init commit

--- src/main/kotlin/io/github/susimsek/springbootgraalvmnativeexample/config/cache/CoCacheManager.kt
+++ src/main/kotlin/io/github/susimsek/springbootgraalvmnativeexample/config/cache/CoCacheManager.kt
@@ -1,10 +1,10 @@
 package io.github.susimsek.springbootgraalvmnativeexample.config.cache
 
 import com.github.benmanes.caffeine.cache.AsyncCache
-import kotlinx.coroutines.Dispatchers
+import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.future.await
-import kotlinx.coroutines.withContext
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentMap
 
 /**
  * A generic coroutine-based cache manager that provides asynchronous caching operations.
@@ -13,6 +13,13 @@ open class CoCacheManager<K, V>(
     private val cache: AsyncCache<K, V>
 ) : CacheManager<K, V> {
 
+    private lateinit var cacheMap: ConcurrentMap<K, CompletableFuture<V>>
+
+    @PostConstruct
+    fun initCache() {
+        cacheMap = cache.asMap()
+    }
+
     /**
      * Retrieves a cached value asynchronously.
      *
@@ -20,7 +27,7 @@ open class CoCacheManager<K, V>(
      * @return the cached value or null if not present
      */
     override suspend fun get(key: K): V? {
-        return cache.getIfPresent(key)?.await()
+        return cacheMap[key]?.await()
     }
 
     /**
@@ -30,7 +37,7 @@ open class CoCacheManager<K, V>(
      * @param value the value to be cached
      */
     override suspend fun put(key: K, value: V) {
-        cache.put(key, CompletableFuture.completedFuture(value))
+        cacheMap[key] = CompletableFuture.completedFuture(value)
     }
 
     /**
@@ -39,17 +46,13 @@ open class CoCacheManager<K, V>(
      * @param key the key whose mapping is to be removed from the cache
      */
     override suspend fun evict(key: K) {
-        withContext(Dispatchers.IO) {
-            cache.synchronous().invalidate(key)
-        }
+        cacheMap.remove(key)
     }
 
     /**
      * Clears all entries from the cache.
      */
     override suspend fun clear() {
-        withContext(Dispatchers.IO) {
-            cache.synchronous().invalidateAll()
-        }
+        cacheMap.clear()
     }
 }
