f871380acad60c0db7aceae8375c9972a456ee3c
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-01-24 11:37:48 +0300

    init commit

--- src/main/kotlin/io/github/susimsek/springbootgraalvmnativeexample/client/TodoClient.kt
+++ src/main/kotlin/io/github/susimsek/springbootgraalvmnativeexample/client/TodoClient.kt
@@ -5,8 +5,16 @@ import kotlinx.coroutines.flow.Flow
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 
+/**
+ * Client interface for interacting with the Todo API.
+ */
 @HttpExchange("/todos")
 interface TodoClient {
+    /**
+     * Fetches a list of todos from the remote API.
+     *
+     * @return a Flow stream of [TodoDTO] representing the todo items.
+     */
     @GetExchange
     fun getTodos(): Flow<TodoDTO>
 }
--- src/main/kotlin/io/github/susimsek/springbootgraalvmnativeexample/service/TodoService.kt
+++ src/main/kotlin/io/github/susimsek/springbootgraalvmnativeexample/service/TodoService.kt
@@ -5,8 +5,16 @@ import io.github.susimsek.springbootgraalvmnativeexample.dto.TodoDTO
 import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 
+/**
+ * Service class for handling Todo operations.
+ */
 @Service
 class TodoService(private val todoClient: TodoClient) {
+    /**
+     * Retrieves the list of todos using the TodoClient.
+     *
+     * @return a Flow stream of [TodoDTO] representing the todo items.
+     */
     fun getTodos(): Flow<TodoDTO> {
         return todoClient.getTodos()
     }
